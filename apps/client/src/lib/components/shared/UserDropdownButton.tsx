0848939ff5ad880d244e3a9b9bc5324bc20bdccf
Styles <kartik100100@gmail.com>
2026-05-20 18:43:59 +0530

    [CLIENT REFACTOR] - Shared UI (apps/client): move custom components batch 5

--- apps/client/src/lib/components/shared/UserDropdownButton.tsx
+++ apps/client/src/lib/components/shared/UserDropdownButton.tsx
@@ -3,7 +3,7 @@ import { useUserProfile } from "@filosign/react/users";
 import { CopySimpleIcon, SignOutIcon, UserIcon } from "@phosphor-icons/react";
 import { motion } from "motion/react";
 import * as React from "react";
-import { Image } from "@/src/lib/components/custom/Image";
+import { Image } from "@/src/lib/components/shared/Image";
 import { Button } from "@/src/lib/components/ui/button";
 import {
 	DropdownMenu,

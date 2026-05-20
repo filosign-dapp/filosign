2a11fd6ac60bc5e5171d0d9ad256e7ba5d411d87
Styles <kartik100100@gmail.com>
2026-05-20 18:44:00 +0530

    [CLIENT REFACTOR] - Dashboard (apps/client): document list under _shell

--- apps/client/src/routes/dashboard/document/all/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import DocumentAllPage from "@/src/pages/dashboard/document/all";
-
-export const Route = createFileRoute("/dashboard/document/all/")({
-	component: DocumentAllPage,
-});

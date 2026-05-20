f679f3fb2a4a277f6bf8e2f525db91048649816a
Styles <kartik100100@gmail.com>
2026-05-20 18:44:00 +0530

    [CLIENT REFACTOR] - Dashboard (apps/client): connections under _shell

--- apps/client/src/routes/dashboard/connections/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import ConnectionsPage from "@/src/pages/dashboard/connections";
-
-export const Route = createFileRoute("/dashboard/connections/")({
-	component: ConnectionsPage,
-});

77183e500c3e3bb836bd59f484a659f1c72148d7
Spandan Barve <spandan567@gmail.com>
2026-05-21 12:01:03 +0530

    added rule errors for cancellation, expiry and token issues

--- apps/contracts/src/errors/EFSPaymentValidator.sol
+++ apps/contracts/src/errors/EFSPaymentValidator.sol
@@ -2,8 +2,15 @@
 pragma solidity ^0.8.26;
 
 error RuleAlreadyExecuted();
+error RuleAlreadyCancelled();
 error RuleNotExecutable();
+error RuleExpired();
 error InvalidPayer();
 error InvalidAmount();
+error InvalidExpiry();
 error InvalidReleaseConfig();
 error UnauthorizedRuleRegistration();
+error TokenNotAllowed();
+error InvalidToken();
+error FileNotRegistered();
+error OnlyServer();

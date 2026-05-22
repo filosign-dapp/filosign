45cfbf66ddd4e188dcc23d1ab847bbc218a368c2
Styles <kartik100100@gmail.com>
2026-05-22 18:25:57 +0530

    [SETTLEMENTS RENAME] - SDK (packages/react-sdk): remove payment hooks and lib modules.

--- packages/react-sdk/src/lib/payment-types.ts
+++
@@ -1,5 +0,0 @@
-import type { InferClientOutputs } from "@orpc/client";
-import type { AppRouterClient } from "../orpc/app-router-types";
-
-export type PaymentRuleRow =
-	InferClientOutputs<AppRouterClient>["payments"]["listByFile"][number];

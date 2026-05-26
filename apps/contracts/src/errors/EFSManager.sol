e5558a53f6885345ee2cc9fc57d52e918dbcfbfa
Styles <kartik100100@gmail.com>
2026-05-26 19:21:02 +0530

    [CONTRACTS V1] - Remove FSManager and FSKeyRegistry (apps/contracts): contracts and errors.

--- apps/contracts/src/errors/EFSManager.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.26;
-
-error SenderAlreadyApproved();
-error CannotApproveSelf();
-error SenderNotApproved();
-error InvalidApproveSignature();
-error InvalidApproveNonce();
-error ApproveSignatureExpired();

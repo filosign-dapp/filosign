eee38114ef3bbd3f64fc1814c957643943d0a130
Styles <kartik100100@gmail.com>
2026-06-05 10:19:59 +0530

    [LANDING PAGE] - CTA Buttons (apps/astro): change request access to start free trial

--- apps/astro/src/lib/marketing-cta.ts
+++ apps/astro/src/lib/marketing-cta.ts
@@ -2,7 +2,7 @@ import { env } from "../env";
 
 export const MARKETING_CTA = {
 	getStartedHref: "/pricing",
-	getStartedLabel: "Request access",
+	getStartedLabel: "Start Free Trial",
 	tryFilosignLabel: "Try the sandbox",
 	sandboxUrl: env.PUBLIC_SANDBOX_CLIENT_URL.replace(/\/$/, ""),
 } as const;
